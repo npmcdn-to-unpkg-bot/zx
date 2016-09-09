bd3cccecf19132fa9a573f03f6f14503830c32b2
lurongze <1946755280@qq.com>
2016-09-09 10:41:07 +0800

    2016.09.10-微信jssdk

--- frontend/widgets/views/wxjs.php
+++ frontend/widgets/views/wxjs.php
@@ -11,6 +11,16 @@ WxAsset::register($this);
 
 ?>
 <script>
+
+var shareItems={
+    title:  '<?=\Yii::$app->params['wxShareTitle']?>',
+    desc:   '<?=\Yii::$app->params['wxShareDesc']?>',
+    link:   '<?=\Yii::$app->params['wxShareLink']?\Yii::$app->params['wxShareLink']:\Yii::$app->request->absoluteUrl?>',
+    imgUrl: '<?=\Yii::$app->params['wxShareImg']?>'
+    ftitle: '<?=\Yii::$app->params['wxShareTitle']?>',
+};
+
+
 wx.config({
     debug: <?=$debug?>, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: "<?=$signPackage['appId']?>", // 必填，公众号的唯一标识
@@ -36,7 +46,7 @@ wx.ready(function(){
     *所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。
     * 对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     * */
-    zxWxShare('<?=\Yii::$app->params['wxShareTitle']?>','<?=\Yii::$app->params['wxShareLink']?>','<?=\Yii::$app->params['wxShareImg']?>');
+    zxWxShare(shareItems);
 
 
 });
@@ -45,17 +55,17 @@ wx.ready(function(){
 * success,cancel 可以传入function(){}
 *type,dataUrl 是分享给朋友的时候是music或者video的才需要填
 * */
-function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
+function zxWxShare(shareItems,success,cancel,type,dataUrl)
 {
     /*分享到朋友圈*/
     wx.onMenuShareTimeline({
-        title: title, // 分享标题
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
+        title: shareItems.title, // 分享标题
+        link: shareItems.link, // 分享链接
+        imgUrl: shareItems.imgUrl, // 分享图标
         success: function () {
             // 用户确认分享后执行的回调函数
             success();
-            zxWxShareCount('Timeline');
+            wxShareCount('Timeline');
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
@@ -65,16 +75,16 @@ function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
 
     /*分享给朋友*/
     wx.onMenuShareAppMessage({
-        title: title, // 分享标题
-        desc: desc, // 分享描述
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
+        title: shareItems.title, // 分享标题
+        desc: shareItems.desc, // 分享描述
+        link: shareItems.link, // 分享链接
+        imgUrl: shareItems.imgUrl, // 分享图标
         type: type, // 分享类型,music、video或link，不填默认为link
         dataUrl: dataUrl, // 如果type是music或video，则要提供数据链接，默认为空
         success: function () {
             // 用户确认分享后执行的回调函数
             success();
-            zxWxShareCount('AppMessage');
+            wxShareCount('AppMessage');
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
@@ -84,14 +94,14 @@ function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
 
     /*分享到QQ*/
     wx.onMenuShareQQ({
-        title: title, // 分享标题
-        desc: desc, // 分享描述
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
+        title: shareItems.title, // 分享标题
+        desc: shareItems.desc, // 分享描述
+        link: shareItems.link, // 分享链接
+        imgUrl: shareItems.imgUrl, // 分享图标
         success: function () {
             // 用户确认分享后执行的回调函数
             success();
-            zxWxShareCount('QQ');
+            wxShareCount('QQ');
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
@@ -101,14 +111,14 @@ function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
 
     /*分享到腾讯微博*/
     wx.onMenuShareWeibo({
-        title: title, // 分享标题
-        desc: desc, // 分享描述
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
+        title: shareItems.title, // 分享标题
+        desc: shareItems.desc, // 分享描述
+        link: shareItems.link, // 分享链接
+        imgUrl: shareItems.imgUrl, // 分享图标
         success: function () {
             // 用户确认分享后执行的回调函数
             success();
-            zxWxShareCount('Weibo');
+            wxShareCount('Weibo');
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
@@ -118,14 +128,14 @@ function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
 
     /**/
     wx.onMenuShareQZone({
-        title: title, // 分享标题
-        desc: desc, // 分享描述
-        link: link, // 分享链接
-        imgUrl: imgUrl, // 分享图标
+        title: shareItems.title, // 分享标题
+        desc: shareItems.desc, // 分享描述
+        link: shareItems.link, // 分享链接
+        imgUrl: shareItems.imgUrl, // 分享图标
         success: function () {
             // 用户确认分享后执行的回调函数
             success();
-            zxWxShareCount('QZone');
+            wxShareCount('QZone');
         },
         cancel: function () {
             // 用户取消分享后执行的回调函数
@@ -137,8 +147,37 @@ function zxWxShare(title,desc,link,imgUrl,success,cancel,type,dataUrl)
 /*
 * 分享统计
 * */
-function zxWxShareCount(type)
+function wxShareCount(type)
 {
 
 }
+
+/*
+* 显示预览图片,把图片加上一个类名(wx-image)
+* */
+function wxPreviewImgs(){
+
+    $(document).on("click",".wx-image",function(){
+        var wxpreview_current_image;
+        var wxpreview_images_list="[";
+
+
+        wxpreview_current_image=$(this).attr("src");
+
+        $.each($(".wx-image"),function(){
+            wxpreview_images_list+=$(this).attr("src")+",";
+        });
+
+        wxpreview_images_list+="]";
+
+        wx.previewImage({
+            current: wxpreview_current_image, // 当前显示图片的http链接
+            urls: wxpreview_images_list // 需要预览的图片http链接列表
+        });
+    });
+
+
+
+}
+
 </script>
